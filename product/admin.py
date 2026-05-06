70d56661a123c476bf12f8b37c85e38857893a74
abbscbn <abbscbn@gmail.com>
2026-05-06 19:04:38 +0300

    admin kullanıcısnın oluşturulması ve modellerin bağlanması

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from product.models import Category
+
 # Register your models here.
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title','status','image']
+    list_filter = ['status']
+
+admin.site.register(Category, CategoryAdmin)
