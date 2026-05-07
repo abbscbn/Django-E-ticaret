dd0b227d37b8d60cc8003186eeaf435e3e0bdca5
abbscbn <abbscbn@gmail.com>
2026-05-08 00:02:54 +0300

    Sitenin meta etiketleri için Settings modeli tanımlandı ve dinamik beslendi alanlar

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from home.models import Setting
+
 # Register your models here.
+
+
+
+class SettingsAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+
+admin.site.register(Setting,SettingsAdmin)
--- home/views.py
+++ home/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from home.models import Setting
+
+
 # Create your views here.
 def index(request):
-
-    return render(request,'home/index.html')
+    setting = Setting.objects.get(pk=1)
+    context = {'setting':setting}
+    return render(request,'home/index.html',context)
